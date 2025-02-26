a0b25b99e2cbb1b876ff739af07f38609d0e3b98
Giovanni <dibellogio@gmail.com>
2025-02-26 16:31:51 +0100

    add post detail page

---
+++ src/pages/PostDetailPage.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
+export default function PostDetailPage() {
+
+    // destrutturo per ritornare l'id tramite param
+    const { id } = useParams();
+
+    //  settaggio dello stato del componente
+    const [post, setPost] = useState({});
+
+    // funzione di chiamata verso la rotta store
+    function fetchPost() {
+        axios.get(`http://localhost:3000/posts/${id}`)
+            .then(res => setPost(res.data))
+
+
+            .catch(err => console.log(err))
+    }
+
+    useEffect(
+        () => fetchPost(),
+        [])
+
+
+    return (
+        <>
+            <div className="postdetail">
+                <h1>{post.title}</h1>
+                <img src={post.image} alt={post.title} />
+                <p>{post.content}</p>
+            </div>
+        </>
+    );
+}
