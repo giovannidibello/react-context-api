0e038940f33dce00af4c7a65c8388ccdb58a10ed
Giovanni <dibellogio@gmail.com>
2025-02-26 16:33:11 +0100

    add route post detail page

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import DefaultLayout from "./layouts/DefaultLayout";
 // Pages
 import HomePage from "./pages/HomePage";
 import PostsPage from "./pages/PostsPage";
-// import PostDetailPage from "./components/PostCard";
+import PostDetailPage from "./pages/PostDetailPage";
 
 function App() {
 
@@ -47,7 +47,7 @@ function App() {
             <Route element={<DefaultLayout />} >
               <Route path="/" element={<HomePage />} />
               <Route path="/posts" element={<PostsPage />} />
-              {/* <Route path="/posts/:id" element={<PostDetailPage />} /> */}
+              <Route path="/posts/:id" element={<PostDetailPage />} />
             </Route>
           </Routes>
         </BrowserRouter>
